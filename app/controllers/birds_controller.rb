c60ab24181e40d2d213cfdc30f4a9d6ffdeaff94
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-07-21 03:29:48 +0300

    completed succesfuly

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,14 @@ class BirdsController < ApplicationController
     end
   end
 
+  #Create /birds
+
+  def create
+    bird = Bird.create({
+      name: params[:name],
+      species: params[:species]
+    })
+    render json: bird, status: :created
+  end 
+
 end
